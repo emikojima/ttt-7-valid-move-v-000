500e16b5d85add2da4fd04926839d9ab2efad0d4
emi kojima <emi8emi@gmail.com>
2018-02-05 18:17:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,7 @@
 
 
 def valid_move?(board,index)
-  if index.between?(0,8) 
-   return true
- end 
- if position_taken?(board,index) == false
+  if index.between?(0,8) && position_taken?(board,index) == false
    return true
  else
    return false
